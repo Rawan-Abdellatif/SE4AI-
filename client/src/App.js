9f49fc40a796d0ead6b294c8beaf20e4c7ace6f4
RawanAbdellatif <rawan.k.abdellatif@gmail.com>
2023-06-28 01:07:17 -0700

    setup

---
+++ client/src/App.js
@@ -0,0 +1,20 @@
+// App.js
+import React from "react";
+// import Navbar from "./component/Navbar";
+// import Footer from "./component/Footer";
+// import Homepage from "./component/Homepage";
+// import "./App.css";
+
+const App = () => {
+  return (
+    <div className="app">
+      Hello
+      {/* <Navbar /> */}
+      {/*  <Homepage /> */}
+      {/* <Footer /> */}
+    </div>
+  );
+};
+
+export default App;
+//
