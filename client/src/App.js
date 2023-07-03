7f669cd2e62b60251ec44b4822831760a1a555ab
RawanAbdellatif <rawan.k.abdellatif@gmail.com>
2023-07-03 13:17:21 -0700

    App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,14 @@
 // App.js
 import React from "react";
 import Navbar from "./component/Navbar";
-// import Footer from "./component/Footer";
-// import Homepage from "./component/Homepage";
-
+import Footer from "./component/Footer";
+import Homepage from "./component/Homepage";
 const App = () => {
   return (
     <div className="app">
-      Hello
       <Navbar />
-      {/*  <Homepage /> */}
-      {/* <Footer /> */}
+      <Homepage />
+      <Footer />
     </div>
   );
 };
