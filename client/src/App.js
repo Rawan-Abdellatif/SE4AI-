bc77a1fa61be79c598618973f75c2f7e3b7d0ea5
RawanAbdellatif <rawan.k.abdellatif@gmail.com>
2023-07-04 20:24:21 -0700

    Add Links to the website

--- client/src/App.js
+++ client/src/App.js
@@ -5,6 +5,7 @@ import Navbar from "./component/Navbar";
 import Homepage from "./component/Homepage";
 import Program from "./component/Program";
 import Events from "./component/Events";
+import Partners from "./component/Partners";
 const App = () => {
   return (
     <BrowserRouter>
@@ -13,6 +14,7 @@ const App = () => {
         <Route path="/" element={<Homepage />} />
         <Route path="/intro" element={<Program />} />
         <Route path="/events" element={<Events />} />
+        <Route path="/partners" element={<Partners />} />
       </Routes>
     </BrowserRouter>
   );
