1998ae74e22efbd98fc7f5ac92cb5a6dbdfaf62b
RawanAbdellatif <rawan.k.abdellatif@gmail.com>
2023-07-13 11:30:14 -0700

    add Alumni to Teams page

--- client/src/component/Alumni.js
+++ client/src/component/Alumni.js
@@ -1,11 +1,7 @@
 // This is the ProgramLeaderShip part in Team  Page
 import React from "react";
 import styled from "styled-components";
-//import  icons
-import { FaArrowUpRightFromSquare } from "react-icons/fa6";
-import { AiFillLinkedin } from "react-icons/ai";
-import { BsTwitter } from "react-icons/bs";
-import { MdEmail } from "react-icons/md";
+
 //import images
 import ErnestoLangOreamunoImage from "../component/team/photos/Ernesto-Lang-Oreamuno.jpg";
 
@@ -22,8 +18,8 @@ const Alumni = () => {
 export default Alumni;
 
 const Img = styled.img`
-  width: 230px;
-  height: 230px;
+  width: 270px;
+  height: 250px;
   border-top-left-radius: 10px;
   border-top-right-radius: 10px;
 `;
@@ -34,7 +30,7 @@ const MiniContainer = styled.div`
   border: 2px solid #f4f0ec;
   padding-top: 0px;
   width: 270px;
-  height: 400px;
+  height: 350px;
   &.emadContainer {
     height: 450px;
   }
@@ -49,53 +45,4 @@ const Name = styled.div`
 
 const University = styled.div`
   margin-left: 15px;
-`;
-
-const Links = styled.div`
-  color: #404040;
-  font-weight: bold;
-  padding-right: 40px;
-
-  ul {
-    list-style: none;
-    margin: 0;
-    padding: 0;
-    color: #404040;
-  }
-  /* li {
-    width: 80px;
-  } */
-`;
-
-const SocialMedia = styled.div`
-  padding-top: 10px;
-  display: flex;
-  align-items: center;
-  flex-wrap: wrap;
-  list-style: none;
-  height: 50px;
-  margin-left: 15px;
-
-  a {
-    display: flex;
-    /* align-items: center; */
-    text-align: center;
-    text-decoration: none;
-    color: #404040;
-    padding: 5px 5px;
-    transition: background-color 0.3s;
-    border-radius: 5px;
-    /* margin-right: 5px; */
-  }
-
-  a:hover {
-    background-color: #e8e8e8;
-  }
-`;
-
-const SmallIcon = styled.span`
-  font-size: 20px;
-  color: #585858;
-  font-weight: bold;
-  margin-right: 7px;
 `;
