f7a75da42a2e2005aa6b61aaed28f574a7c38ede
RawanAbdellatif <rawan.k.abdellatif@gmail.com>
2023-08-04 22:23:53 -0700

    add login to the navbar

--- client/src/component/Navbar.js
+++ client/src/component/Navbar.js
@@ -1,16 +1,32 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import styled from "styled-components";
 import logo from "./img/logo.svg";
 import logoDark from "./img/logo_dark.svg";
 import { DarkModeContext } from "./DarkModeContext";
+import { Link, useNavigate } from "react-router-dom";
 
 const Navbar = () => {
   const { toggleDarkMode, isDarkMode } = useContext(DarkModeContext);
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [selectedOption, setSelectedOption] = useState(null);
+  const navigate = useNavigate();
 
+  const handleLoginToggle = () => {
+    setShowDropdown(!showDropdown); // Toggle the visibility of the dropdown
+  };
+
+  const handleDropdownOptionClick = (option) => {
+    setSelectedOption(option);
+    setShowDropdown(false); // Close the dropdown when an option is selected
+  };
   const handleToggle = () => {
     toggleDarkMode(); // Toggle the dark mode state in the context
   };
 
+  const handleAdminClick = () => {
+    // Use navigate to redirect to the /adminlogin route
+    navigate("/adminlogin");
+  };
   return (
     // <NavbarStickyContainer>
     <NavbarContainer isDarkMode={isDarkMode}>
@@ -40,6 +56,25 @@ const Navbar = () => {
         </NavMenuItem>
         <NavMenuItem isDarkMode={isDarkMode}>
           <a href="/apply">Apply</a>
+        </NavMenuItem>
+        <NavMenuItem isDarkMode={isDarkMode}>
+          {/* Click event for "Login" */}
+          <LoginLink onClick={handleLoginToggle}>Login</LoginLink>
+
+          {/* Dropdown menu */}
+          {showDropdown && (
+            <DropdownMenuWrapper isDarkMode={isDarkMode}>
+              <DropdownMenu isDarkMode={isDarkMode}>
+                <DropdownItem onClick={handleAdminClick}>Admin</DropdownItem>
+
+                <DropdownItem
+                  onClick={() => handleDropdownOptionClick("member")}
+                >
+                  Member
+                </DropdownItem>
+              </DropdownMenu>
+            </DropdownMenuWrapper>
+          )}
         </NavMenuItem>
 
         <NavMenuItem isDarkMode={isDarkMode}>
@@ -77,25 +112,21 @@ const NavbarContainer = styled.nav`
   height: 45px;
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  /* justify-content: space-between; */
   font-weight: bold;
   font-size: 12px;
   margin-left: -10px;
-  margin-right: -10px;
+  /* margin-right: -10px; */
   padding: 5px 0px;
   font-family: "Open Sans", sans-serif;
   background-color: ${(props) => (props.isDarkMode ? "#282828" : "white")};
   /* border-bottom: 1px solid #dcdcdc; */
   position: ${(props) => (props.scrollPosition > 20 ? "sticky" : "relative")};
   top: ${(props) => (props.scrollPosition > 20 ? "20px" : "0")};
-  z-index: 100;
+  z-index: 1;
   transition: top 0.3s ease; /* Add a transition for smooth movement */
 `;
-const NavbarStickyContainer = styled.div`
-  position: sticky;
-  top: 0;
-  z-index: 1;
-`;
+
 const NavMenu = styled.ul`
   display: flex;
   padding-top: 10px;
@@ -104,28 +135,26 @@ const NavMenu = styled.ul`
 const Nav = styled.li`
   /* margin-right: 3px; */
   display: flex;
-  width: 120px;
+  margin-left: -30px;
 
   img {
     height: 40px;
     cursor: pointer;
     margin-top: -10px;
-    margin-left: -40px;
-    padding-left: 5px;
-    margin-right: 3px;
+    /* padding-left: 7px; */
+    /* margin-right: 3px; */
     /* width: 20px; */
   }
   a {
     /* margin-top: -10px; */
     align-items: vertical;
-
     color: ${(props) => (props.isDarkMode ? "#fff" : "#212529")};
     text-decoration: none;
     font-size: 15px;
     font-weight: bolder;
     padding-top: 1px;
-    /* width: 80px; */
-    padding-left: 4px;
+    width: 120px;
+    margin-left: -40px;
     /* padding-right: -3px; */
     transition: 0.2s;
     text-align: center;
@@ -136,15 +165,17 @@ const Nav = styled.li`
 `;
 
 const NavMenuItem = styled.li`
-  margin-right: 10px;
+  /* margin-right: 10px; */
+  text-align: center;
+  margin-left: 25px;
+  /* width: 90px; */
   display: flex;
-  /* margin-left: 4px; */
-
+  /* border: 1px solid green; */
   a {
     /* color: #fff; */
     text-decoration: none;
     font-size: 16px;
-    padding-left: 6px;
+    padding-left: 2px;
     padding-right: 2px;
 
     transition: 0.2s;
@@ -159,7 +190,7 @@ const NavMenuItem = styled.li`
 `;
 
 const Contact = styled.div`
-  padding-left: 490px;
+  padding-left: 350px;
 `;
 
 const SwitchLabel = styled.label`
@@ -226,5 +257,42 @@ const Slider = styled.span`
 
   &.active:before {
     transform: translateX(26px);
+  }
+`;
+const LoginLink = styled.a`
+  cursor: pointer;
+
+  // Add any additional styling for the login link here if needed
+`;
+
+const DropdownMenuWrapper = styled.div`
+  position: relative;
+  display: inline-block;
+  margin-left: -70px;
+`;
+
+const DropdownMenu = styled.div`
+  position: absolute;
+  top: 100%;
+  left: 0;
+  /* border: 1px solid #dcdcdc; */
+  border-radius: 4px;
+  padding: 8px;
+  z-index: 1; /* Ensure the dropdown menu appears above other elements */
+  background-color: ${(props) => (props.isDarkMode ? "#282828" : "white")};
+  color: ${(props) => (props.isDarkMode ? "#fff" : "#212529")};
+`;
+
+const DropdownItem = styled.div`
+  cursor: pointer;
+  font-size: 16px;
+
+  padding: 6px 12px;
+  /* color: ${(props) => (props.isDarkMode ? "#fff" : "#212529")}; */
+  /* transition: background-color 0.2s; */
+
+  &:hover {
+    background-color: #f0f0f0;
+    color: #db7093;
   }
 `;
